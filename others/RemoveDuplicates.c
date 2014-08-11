12cc9a34a5a4577960f4432d541e46bc0292dcd0
Prateek Sharma <prateek123.shm@gmail.com>
2014-08-11 12:15:52 +0530

    Removing duplicates in a sorted array completed

---
+++ others/RemoveDuplicates.c
@@ -0,0 +1,25 @@
+//Remove duplicated in an array
+#include <stdio.h>
+
+int main()
+{
+	//int a[]={1,1,2,3,4,4,5,5,5,6};
+	int a[]={1,1,1,1,1,1,1,1,1,1};
+	int i=0,j=0;
+	
+	while(i<10 && j<10)
+	{
+		while(a[i]==a[j])
+		{
+			j++;
+		}
+		i++;
+		a[i]=a[j];
+	}
+	
+	for(j=0;j<i;j++)
+	{
+		printf("%d ",a[j]);
+	}
+	return 0;
+}
