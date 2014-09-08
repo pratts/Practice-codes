c234a147580645363bd87ddbff49f39f97a1b546
Prateek Sharma <prateek123.shm@gmail.com>
2014-09-08 14:52:08 +0530

    fork source included

--- OS/ForkExample.c
+++ OS/ForkExample.c
@@ -1,11 +1,16 @@
-#include <stdio.h>
+//Fork's example
+//Source : man fork , http://stackoverflow.com/questions/1653340/differences-between-exec-and-fork
+//Zombie process : http://www.howtogeek.com/119815/htg-explains-what-is-a-zombie-process-on-linux/,http://askubuntu.com/questions/48624/what-are-zombie-processes
 #include <unistd.h>
+#include <stdio.h>
 
-int main()
-{
-	pid_t a=fork();
-	pid_t b=fork();
-	printf("%d %d",a,b);
-	
-	return 0;
+int main(void) {
+    int i;
+    for (i= 0; i < 3; ++i) {
+        int f = fork();
+        printf("%i\tspawns\t%i\ti=%i\n", getpid(), f, i);
+        if (f && (i == 1))
+            break;
+    }
+    return 0;
 }
