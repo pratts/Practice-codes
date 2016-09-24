000c4e241e4a34184ab7eb341235622caea9d7f4
Prateek Sharma <prateek.usict@gmail.com>
2016-09-25 00:19:20 +0530

    set n bits at p in x to n rightmost bits in y

---
+++ cprog/setBits.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void setBits(unsigned int x, int p, int n, unsigned int y) {
+	y = (~(~0 << n) & y) << (p - n);
+	x = x & ~((~(~0 << n)) << (p - n));
+
+	printf("%d\n", x| y);
+}
+
+int main(int argc, char **argv) {
+	setBits(181, 4, 3, 3);
+	return 0;
+}
